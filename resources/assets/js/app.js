d3502aa3411f61c33d77e980518e5bbc6837c49e
Jim Wright <jmwri93@gmail.com>
2017-11-07 17:55:00 +0000

    Remove vue-material

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,16 +1,7 @@
-
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
 require('./bootstrap');
 
 window.Vue = require('vue');
-const VueMaterial = require('vue-material');
 
-Vue.use(VueMaterial);
 Vue.component('example', require('./components/Example.vue'));
 
 const app = new Vue({
