2f7762c6db55998f708298d8fcf79f03d88c98af
Jim Wright <jmwri93@gmail.com>
2018-06-21 18:56:32 +0100

    Use API to get dashboard stats

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -12,7 +12,9 @@
                 <h3 class="display-3">RuneHistory</h3>
                 <span class="subheading">Old School RuneScape XP tracker</span>
                 <v-divider class="my-3"></v-divider>
-                <div class="title mb-3">Join 999 other players and start tracking your account today!</div>
+                <div class="title mb-3">Join {{ accountCount }} other players and start tracking
+                  your account
+                  today!</div>
                 <v-btn large color="primary" class="mx-0" :to="{ name: 'search' }">Get started</v-btn>
               </v-flex>
             </v-layout>
@@ -35,8 +37,9 @@
               <v-flex xs12 sm4>
                 <v-card color="blue-grey darken-2" class="white--text">
                   <v-card-title primary-title>
-                    <div class="headline">999 players tracked</div>
-                    <div>We are actively tracking 999 players XP up to every 10 minutes.</div>
+                    <div class="headline">{{ accountCount }} players tracked</div>
+                    <div>We are actively tracking {{ accountCount }} players XP up to every 10
+                      minutes.</div>
                   </v-card-title>
                   <v-card-actions>
                     <v-btn flat dark :to="{ name: 'search' }">Track your account</v-btn>
@@ -46,8 +49,10 @@
               <v-flex xs12 sm4 lg4>
                 <v-card color="cyan darken-2" class="white--text">
                   <v-card-title primary-title>
-                    <div class="headline">999,999 data points</div>
-                    <div>Since RuneHistory launched in 2018, we have gathered 999,999 data points.</div>
+                    <div class="headline">{{ highScoreCount }} data points</div>
+                    <div>Since RuneHistory launched in 2018, we have gathered {{ highScoreCount
+                      }} data points
+                      .</div>
                   </v-card-title>
                   <v-card-actions>
                     <v-btn flat dark :to="{ name: 'search' }">Track your account</v-btn>
@@ -73,7 +78,43 @@
   </v-container>
 </template>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<script>
+  import rh from '../client'
 
-</style>
+  export default {
+    created() {
+      this.loadAccountCount()
+      this.loadHighScoreCount()
+    },
+    data() {
+      return {
+        accountCount: null,
+        highScoreCount: null,
+      }
+    },
+    methods: {
+      loadAccountCount() {
+        return rh.stats().count_accounts()
+          .then((count) => {
+            this.accountCount = count
+            return count
+          })
+          .catch((err) => {
+            this.accountCount = null
+            throw err
+          })
+      },
+      loadHighScoreCount() {
+        return rh.stats().count_highscores()
+          .then((count) => {
+            this.highScoreCount = count
+            return count
+          })
+          .catch((err) => {
+            this.highScoreCount = null
+            throw err
+          })
+      },
+    },
+  }
+</script>
